893d23f0cd339efa2e68bca6142f82a7671b3c31
Mislav Srecec <mislav.srecec3@gmail.com>
2020-10-07 11:39:29 +0200

    Finished app and added form clear

--- src/components/learning-resources/StoredResources.vue
+++ src/components/learning-resources/StoredResources.vue
@@ -3,6 +3,7 @@
     <learning-resource
       v-for="res in resources"
       :key="res.id"
+      :id="res.id"
       :title="res.title"
       :description="res.description"
       :link="res.link"
@@ -15,10 +16,10 @@
 import LearningResource from './LearningResource.vue';
 
 export default {
+  inject: ['resources'],
   components: {
     LearningResource
-  },
-  props: ['resources']
+  }
 };
 </script>
 
